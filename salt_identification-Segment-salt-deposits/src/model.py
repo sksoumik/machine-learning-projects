819fa08978ee5decfe505441805b33f30da3f3a9
Sadman Kabir Soumik <sadmanks@gmail.com>
2020-02-10 11:50:26 +0600

    UNet1 model

--- salt_identification-Segment-salt-deposits/src/model.py
+++ salt_identification-Segment-salt-deposits/src/model.py
@@ -1,3 +1,8 @@
+"""
+UNet11 Model codes are copied from the following git repo: 
+https://github.com/ternaus/TernausNet/blob/master/unet_models.py
+"""
+
 import torch
 from torch import nn
 from torch.nn import functional as F
@@ -45,4 +50,62 @@ class UNet11(nn.Module):
         super().__init__()
         self.pool = nn.MaxPool2d(2, 2)
         self.encoder = models.vgg11().features
-        
+        self.relu = self.encoder[1]
+        self.conv1 = self.encoder[0]
+        self.conv2 = self.encoder[3]
+        self.conv3s = self.encoder[6]
+        self.conv3 = self.encoder[8]
+        self.conv4s = self.encoder[11]
+        self.conv4 = self.encoder[13]
+        self.conv5s = self.encoder[16]
+        self.conv5 = self.encoder[18]
+
+        # DecoderBlock(in_channels, middle_channels, out_channels)
+        self.center = DecoderBlock(num_filters * 8 * 2, num_filters * 8 * 2,
+                                   num_filters * 8)
+        self.dec5 = DecoderBlock(num_filters * (16 + 8), num_filters * 8 * 2,
+                                 num_filters * 8)
+        self.dec4 = DecoderBlock(num_filters * (16 + 8), num_filters * 8 * 2,
+                                 num_filters * 4)
+        self.dec3 = DecoderBlock(num_filters * (8 + 4), num_filters * 4 * 2,
+                                 num_filters * 2)
+        self.dec2 = DecoderBlock(num_filters * (4 + 2), num_filters * 2 * 2,
+                                 num_filters)
+        self.dec1 = ConvRelu(num_filters * (2 + 1), num_filters)
+
+        self.final = nn.Conv2d(
+            num_filters,
+            1,
+            kernel_size=1,
+        )
+
+    def forward(self, x):
+        conv1 = self.relu(self.conv1(x))
+        conv2 = self.relu(self.conv2(self.pool(conv1)))
+        conv3s = self.relu(self.conv3s(self.pool(conv2)))
+        conv3 = self.relu(self.conv3(conv3s))
+        conv4s = self.relu(self.conv4s(self.pool(conv3)))
+        conv4 = self.relu(self.conv4(conv4s))
+        conv5s = self.relu(self.conv5s(self.pool(conv4)))
+        conv5 = self.relu(self.conv5(conv5s))
+
+        center = self.center(self.pool(conv5))
+
+        # Deconvolutions with copies of VGG11 layers of corresponding size
+        dec5 = self.dec5(torch.cat([center, conv5], 1))
+        dec4 = self.dec4(torch.cat([dec5, conv4], 1))
+        dec3 = self.dec3(torch.cat([dec4, conv3], 1))
+        dec2 = self.dec2(torch.cat([dec3, conv2], 1))
+        dec1 = self.dec1(torch.cat([dec2, conv1], 1))
+        return F.sigmoid(self.final(dec1))
+
+
+def unet11(**kwargs):
+    model = UNet11(**kwargs)
+    return model
+
+
+def get_model():
+    model = unet11()
+    model.train()
+    return model.cuda()
